83a3d2d8117adb1d0e6b6d99a27878d2c9b37ca3
Damiano <fiagemsrl@gmail.com>
2026-05-27 17:42:54 +0200

    add pwa installable app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
